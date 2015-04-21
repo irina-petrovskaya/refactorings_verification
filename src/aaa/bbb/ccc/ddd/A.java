1df1a9b0610320349bcd0d962e965c34d099cb8a
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2015-04-21 17:06:45 +0300

    rename arg

--- src/aaa/bbb/ccc/ddd/A.java
+++ src/aaa/bbb/ccc/ddd/A.java
@@ -3,9 +3,10 @@ package aaa.bbb.ccc.ddd;
 import java.lang.Override;import java.lang.Runnable;import java.lang.String;import java.lang.System;import java.util.List;
 
 public class A {
-    public void example(List<String> list, String value)
+    public void example(List<String> list, String string)
     {
-        list.stream().filter(listValue -> value.equals(listValue)); // incorrect lambda replacement
+
+        list.stream().filter(listValue -> string.equals(listValue)); // incorrect lambda replacement
     }
     void b() {
         Runnable c = new Runnable() {
