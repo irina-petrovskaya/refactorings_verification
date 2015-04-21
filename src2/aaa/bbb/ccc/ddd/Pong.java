f41607011fe9c82dfa325192ba1deca617e7b114
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2015-04-21 18:03:02 +0300

    comments

--- src2/aaa/bbb/ccc/ddd/Pong.java
+++ src2/aaa/bbb/ccc/ddd/Pong.java
@@ -1,5 +1,7 @@
 package aaa.bbb.ccc.ddd;
-
+// this test doesn't compile!!!
+// also, inspections hang up
+// previously - SOE in analysis performing
 interface Pong<T> {}
 class Ping<T> implements Pong<Pong<? super Ping<Ping<T>>>> {
     static void ping() {
