4a97dcd755a234125748eeeb4a89c2c016702c57
irina <irina@me.com>
2016-02-17 15:58:14 +0300

    toString();

--- src/aaa/bbb/ccc/ddd/AAAAA.java
+++ src/aaa/bbb/ccc/ddd/AAAAA.java
@@ -4,6 +4,12 @@ package aaa.bbb.ccc.ddd;
  * Created by user on 17/02/16.
  */
 public class AAAAA {
+    @Override
+    public String toString() {
+        return "AAAAA{" +
+                "foo='" + foo + '\'' +
+                '}';
+    }
 
     String foo;
 }
