57fcb85a8e4cb64b3c85d99b039a8ea270ae3b6f
irina <irina@me.com>
2016-02-17 15:36:04 +0300

    formatting; suppress

--- src/aaa/bbb/ccc/ddd/TestEnum.java
+++ src/aaa/bbb/ccc/ddd/TestEnum.java
@@ -2,9 +2,8 @@ package aaa.bbb.ccc.ddd;
 
 public enum TestEnum {
     @SuppressWarnings("unused")A, @SuppressWarnings("unused")B;
-
     @SuppressWarnings("unused")
-    public void foo(int i) {
+    public void foo(int i){
         System.out.println("hello");
     }
 }
